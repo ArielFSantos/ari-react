fe92139337cfaed55b2b4cfd50484f57700da665
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2023-05-23 23:38:04 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Botao from './botao'
+export default function App() {
+  return(
+      <div>
+        <h1>Ola Mundo, React!</h1>  
+        <Botao conteudo=' clica aqui mano' />
+      </div>
+      
+  )
+}
+
+
---
+++ src/botao.jsx
@@ -0,0 +1,6 @@
+export default function Botao(props){
+  return(   
+      <button>{props.conteudo}</button>  
+    )
+}
+
